94ec9a877425effa8f5597f4e73342c1098f4267
Kinneyzhang <kinneyzhang666@gmail.com>
2022-06-16 21:08:46 +0800

    'update'

--- C-Algorithm/Sort/bubbleSort.c
+++ C-Algorithm/Sort/bubbleSort.c
@@ -1,12 +1,17 @@
 /*
  * 冒泡排序
+ * 简单描述：每次将最小的元素冒泡(连续交换)到开头。
+ * 描述：
+ *   从最后一个元素(j)遍历到第i+1(i from 0 to size)个元素，比较 j 和 j-1 个元素，a[j] < a[j-1] 则交换。每次遍历将最小的元素放（冒泡）到i的位置。
+ * n-1 趟，每趟有从 n-1 到 1 递减的比较（交换）操作
+ * 最左边得到一个依次有序的序列
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 
 void bubbleSort (int a[], int size) {
-  for (int i=0; i<size; i++) {
+  for (int i=0; i<size-1; i++) {
     for (int j=size; j>i; j--) {
       if (a[j] < a[j-1]) {
         int t = a[j];
@@ -14,11 +19,16 @@ void bubbleSort (int a[], int size) {
         a[j-1] = t;
       }
     }
+    /* for (int i=0; i<size; i++) { */
+    /*   printf("%d ", a[i]); */
+    /* } */
+    /* printf("\n"); */
   }
 }
 
 int main () {
-  int a[8] = {4, 2, 5, 7, 1, 3, 9, 0};
+  /* int a[8] = {4, 2, 5, 7, 1, 3, 9, 0}; */
+  int a[8] = {9, 8, 7, 6, 5, 4, 3, 2};
   int size = 8;
 
   printf("Before sort: ");
--- C-Algorithm/Sort/selectSort.c
+++ C-Algorithm/Sort/selectSort.c
@@ -1,11 +1,19 @@
 /*
  * 选择排序
+ * 简单描述：每次选择一个最小的元素，交换到开头。
+ * 描述：
+ *   从第一个元素遍历到最后一个元素，每趟在剩余的元素中依次与当前元素比较，记录最小元素的下标，将当前元素与最小元素交换。
+ * n-1趟，每趟有从 n-1 到 1 递减次的比较操作，最后(交换)一次。
+ * 最左边得到一个依次有序的序列
+ * 每次交换都将一个元素放到它最终的位置上，交换次数是n-1
+ * 缺点：不能利用已排序好的顺序，每次都要比较所有剩余元素才能选出最小。
+ * 优点：比较的次数多(固定)，但交换的次数少(n-1)，适合元素较大(突出交换的优势)，但关键字较小(减少比较的开销)的文件。
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-void selectSort (char a[], int size) {
+void selectSort (int a[], int size) {
   for (int i=0; i<size; i++) {
     int m = i; // position of the minimum element.
     for (int j=i+1; j<size; j++)
@@ -18,15 +26,16 @@ void selectSort (char a[], int size) {
 }
 
 int main () {
-  /* int size = 8; */
   /* int a[8] = {4, 2, 5, 7, 1, 3, 9, 0}; */
+  int a[8] = {9, 8, 7, 6, 5, 4, 3, 2};
+  int size = 8;
 
-  int size = 12;
-  char a[12] = {'E', 'A', 'S', 'Y', 'Q', 'U', 'E', 'S', 'T', 'I', 'O', 'N'};
+  /* int size = 12; */
+  /* char a[12] = {'E', 'A', 'S', 'Y', 'Q', 'U', 'E', 'S', 'T', 'I', 'O', 'N'}; */
   
   printf("Before sort: ");
   for (int i=0; i<size; i++) {
-    printf("%c ", a[i]);
+    printf("%d ", a[i]);
   }
   
   printf("\n");
@@ -34,6 +43,6 @@ int main () {
   selectSort(a, size);
   printf("After sort:  ");
   for (int i=0; i<size; i++) {
-    printf("%c ", a[i]);
+    printf("%d ", a[i]);
   }
 }
