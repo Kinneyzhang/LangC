87d3dc94ddf51b77780bd17aa16b65f58508a0ba
Kinneyzhang <kinneyzhang666@gmail.com>
2022-07-05 22:03:13 +0800

    update

--- C-Leetcode/1-two-sum.c
+++ C-Leetcode/1-two-sum.c
@@ -1,29 +1,20 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
+/* 两数之和
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出和为目标值 target 的那 两个整数，并返回它们的数组下标。你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 
 void twoSum(int* nums, int numsSize, int target, int* returnSize){
-  for(int i=0; i<numsSize; i++) {
-    if(nums[i] > target) continue;
-    else if(nums[i] = target) {
-      *returnSize = nums[i];
-    }
-    else {
-      int a = nums[i];
-      int b = target-a;
-      twoSum(&nums[i+1], numsSize-i-1, b, returnSize);
-    }
-  }
+  
 }
 
 int main() {
-  int nums[4] = {2,7,11,15};
-  int numsSize = 4;
+  int nums[4] = {6,2,7,5,15,4};
+  int numsSize = 6;
   int target = 9;
   int *returnSize = (int*)malloc(sizeof(int));
+  
   twoSum(nums, numsSize, target, returnSize);
   printf("%d\n", *returnSize);
   printf("%d\n", *(returnSize+1));
